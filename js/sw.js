6472f9a3c5bc052cb05030cf250aae7c683415ba
werealtor <werealtor1@icloud.com>
2025-09-21 23:42:14 -0700

    Delete js/sw.js

--- js/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('casei-v1').then(c=>c.addAll([
-    '/', '/index.html', '/prices.json',
-    '/css/style.css', '/js/script.js'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
-});
